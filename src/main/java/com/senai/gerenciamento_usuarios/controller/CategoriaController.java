5b811457186b439777c1081a1582ad3545e83d91
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-04-07 18:07:34 -0300

    feat: cria estrutura base para categoria controller

---
+++ src/main/java/com/senai/gerenciamento_usuarios/controller/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.senai.gerenciamento_usuarios.controller;
+
+import com.senai.gerenciamento_usuarios.dto.CategoriaDto;
+import com.senai.gerenciamento_usuarios.service.CadastroProdutosService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class CategoriaController {
+
+    //Injeção de Dependencias
+    private final CadastroProdutosService cadastroProdutosService;
+
+    public CategoriaController(CadastroProdutosService cadastroProdutosService) {
+        this.cadastroProdutosService = cadastroProdutosService;
+    }
+
+    @PostMapping("/categorias")
+    public ResponseEntity<String> cadastrar(@RequestBody CategoriaDto categoriaDto) {
+        return null;
+    }
+
+    @GetMapping("/categorias")
+    public ResponseEntity<String> listar(@RequestBody CategoriaDto categoriaDto) {
+        return null;
+    }
+}
