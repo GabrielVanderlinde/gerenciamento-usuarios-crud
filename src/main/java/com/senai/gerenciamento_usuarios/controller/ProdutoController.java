b4d6659bc89f25b16f28d6686d2fa48111e49982
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-04-14 21:03:01 -0300

    refactor: atualiza produto controller visando otimizacao, simplificacao e bom uso jpa

--- src/main/java/com/senai/gerenciamento_usuarios/controller/ProdutoController.java
+++ src/main/java/com/senai/gerenciamento_usuarios/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.senai.gerenciamento_usuarios.controller;
 import com.senai.gerenciamento_usuarios.dto.CategoriaDto;
 import com.senai.gerenciamento_usuarios.dto.ProdutoDto;
 import com.senai.gerenciamento_usuarios.service.CadastroProdutosService;
+import com.senai.gerenciamento_usuarios.service.ProdutoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +16,16 @@ import java.util.List;
 public class ProdutoController {
 
     //Injeção de Dependencias
-    private final CadastroProdutosService cadastroProdutosService;
+    private final ProdutoService produtoService;
 
-    public ProdutoController(CadastroProdutosService cadastroProdutosService) {
-        this.cadastroProdutosService = cadastroProdutosService;
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
     }
 
     @PostMapping("/produtos")
     public ResponseEntity<String> cadastrar(@RequestBody ProdutoDto produtoDto) {
 
-        boolean sucesso = cadastroProdutosService.cadastrarProduto(produtoDto);
+        boolean sucesso = produtoService.cadastrarProduto(produtoDto);
 
         if (sucesso) {
             return ResponseEntity.ok("Produto cadastrado com sucesso");
@@ -35,13 +36,15 @@ public class ProdutoController {
 
     @GetMapping("/produtos")
     public ResponseEntity<List<ProdutoDto>> listar() {
-        List<ProdutoDto> lista = cadastroProdutosService.listarProdutos();
+        List<ProdutoDto> lista = produtoService.listarProdutos();
         return ResponseEntity.ok(lista);
     }
 
+    /*
     @GetMapping("/produtos/categoria/{id}")
     public ResponseEntity<List<ProdutoDto>> listarCategoria(@PathVariable Long id) {
-        List<ProdutoDto> lista = cadastroProdutosService.listarProdutosPorCategoria(id);
+        List<ProdutoDto> lista = produtoService.listarProdutosPorCategoria(id);
         return ResponseEntity.ok(lista);
     }
+     */
 }
