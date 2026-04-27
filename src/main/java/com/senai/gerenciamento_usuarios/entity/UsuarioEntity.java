9072805e0f35cf4319d80e675ab59d3028267593
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-04-27 18:48:28 -0300

    feat: implementa logica entity para usuario

--- src/main/java/com/senai/gerenciamento_usuarios/entity/UsuarioEntity.java
+++ src/main/java/com/senai/gerenciamento_usuarios/entity/UsuarioEntity.java
@@ -1,13 +1,22 @@
 package com.senai.gerenciamento_usuarios.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "usuarios")
 public class UsuarioEntity {
 
+    @Id
+    @Column(name = "cpf_usuario")
     private String cpf;
+    @Column(name = "nome_usuario")
     private String nomeCompleto;
+    @Column(name = "login_usuario")
     private String login;
+    @Column(name = "senha_usuario")
     private String senha;
 
 
